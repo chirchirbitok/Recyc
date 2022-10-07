a47ee74bbee4775e20ac7fe4b7538ba98e546bbf
chirchirbitok <douglasbitok@gmail.com>
2022-10-07 10:33:37 +0300

    Inflate new search button to the menu#2

--- app/src/main/java/com/example/recyc/MainActivity.java
+++ app/src/main/java/com/example/recyc/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,5 +46,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mainAdapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflating search xml to Menu
+        getMenuInflater().inflate(R.menu.search,menu);
+        //object of menu item
+        MenuItem menuItem = menu.findItem(R.id.search);
+        //create a search view
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                txtSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                txtSearch(query);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void txtSearch(String str){
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("teachers").orderByChild("name").startAt(str).endAt(str+"~"), MainModel.class)
+                        .build();
+        mainAdapter = new MainAdapter(options);
+        mainAdapter.startListening();
+        //set it now in the recycler view
+        recyclerView.setAdapter(mainAdapter);
     }
 }
