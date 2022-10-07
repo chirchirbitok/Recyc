f1adaf475fdd032e4e52d78476c9d13e52a6214b
chirchirbitok <douglasbitok@gmail.com>
2022-10-07 12:16:31 +0300

    Edit and Delete dialog pop up #1

--- app/src/main/java/com/example/recyc/MainAdapter.java
+++ app/src/main/java/com/example/recyc/MainAdapter.java
@@ -1,9 +1,13 @@
 package com.example.recyc;
 
+import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +15,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,6 +42,8 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
         CircleImageView img;
         TextView name,course,email;
 
+        Button btnEdit, btnDelete;
+
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -37,6 +51,9 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
             name = (TextView) itemView.findViewById(R.id.nametext);
             course = (TextView) itemView.findViewById(R.id.coursetext);
             email = (TextView) itemView.findViewById(R.id.emailtext);
+
+            btnEdit = (Button) itemView.findViewById(R.id.btnEdit);
+            btnDelete = (Button) itemView.findViewById(R.id.btnDelete);
         }
     }
 
@@ -52,6 +69,63 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
                 .circleCrop()
                 .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
                 .into(holder.img);
+
+        //use holder to access and set all the values
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1100)
+                        .create();
+
+                //dialogPlus.show();
+
+                View view = dialogPlus.getHolderView();
+
+                EditText name = view.findViewById(R.id.txtName);
+                EditText email = view.findViewById(R.id.txtEmail);
+                EditText course = view.findViewById(R.id.txtCourse);
+                EditText turl = view.findViewById(R.id.txtImageUrl);
+
+                Button btnUpdate = view.findViewById(R.id.btnUpdate);
+
+                name.setText(model.getName());
+                email.setText(model.getEmail());
+                course.setText(model.getCourse());
+                turl.setText(model.getTurl());
+
+                dialogPlus.show();
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("name",name.getText().toString());
+                        map.put("email",email.getText().toString());
+                        map.put("course",course.getText().toString());
+                        map.put("turl",turl.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("teachers")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(), "Data Updated Successfully", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(holder.name.getContext(), "Error While Updating", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
+
+            }
+        });
     }
 
     @NonNull
